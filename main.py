67c3f003879fc95a500fafec8340f64dab928d26
shchepin85 <43093705+shchepin85@users.noreply.github.com>
2020-02-10 00:02:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from jinja2 import Template
 
-import smtplib                                      # Библиотека для SMTP
-from email.mime.multipart import MIMEMultipart      # Многокомпонентный объект
-from email.mime.text import MIMEText                # Текст/HTML
-from email.mime.image import MIMEImage              # Изображения
-from jinja2 import Template                         # Объект Template из шаблонизатора jinja2
+# ============================================================================================== #
 
-
-# ФУНКЦИЯ - Прочитать шаблон из файла и модифицировать его
-
+# Формируем текст письма по шаблону
 def template_modify(c_name, c_email):
+    
     fileread = open("templates/test.txt", encoding="utf-8").read()
     filemodify = Template(fileread).render(client_name=c_name, client_email=c_email)
     return filemodify
 
-# ФУНКЦИЯ - Отправить письмо
-
+# Отправляем письмо
 def send_email_to_client(addr_from, password, addr_to, email_subject, email_body):
 
     msg            = MIMEMultipart()                        # Создаем сообщение
@@ -24,19 +22,16 @@ def send_email_to_client(addr_from, password, addr_to, email_subject, email_body
     msg.attach(MIMEText(email_body, 'plain'))               # Добавляем в сообщение текст
 
     server = smtplib.SMTP('smtp.gmail.com', 587)            # Создаем объект SMTP
-    ####server.set_debuglevel(True)                         # Включаем режим отладки
     server.starttls()                                       # Начинаем шифрованный обмен по TLS
     server.login(addr_from, password)                       # Получаем доступ
     server.send_message(msg)                                # Отправляем сообщение
-    server.quit()                                           # Выходим
+    server.quit()                                           # Логаут
 
-    return print("Email отправлен!")
+# ============================================================================================== #
 
-# От винта!
-
-send_email_to_client('shchepin85@gmail.com',                # Адресат
-                     'J#QQ$h7h2v2hch4',                     # Пароль
-                     'shchepin85@gmail.com',                # Получатель
+send_email_to_client('Адресат',                             # Адресат
+                     'Пароль',                              # Пароль
+                     'Получатель',                          # Получатель
                      'Тема письма',                         # Тема письма
                      template_modify('Имя клиента',         #
                                      'Емайл клиента'))      # Текст письма
