1925e8dd4a5cac196a3bae9934142e72a29d0f1d
shchepin85 <43093705+shchepin85@users.noreply.github.com>
2020-04-01 13:47:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -31,84 +31,87 @@
  
 import os
 import smtplib
-from   email.mime.multipart import MIMEMultipart
-from   email.mime.text      import MIMEText
-# from email.mime.image     import MIMEImage
-from   jinja2               import Template, Environment, meta 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# from email.mime.image import MIMEImage
+from jinja2 import Template, Environment, meta 
  
  
 # ======================== FUNCTIONS ========================= #
- 
-# Select template from the template directory
+
 def shmailer_template_select():
+    '''Select template from the directory'''
  
-    template_files_directory      = os.path.join(os.getcwd(),'templates')
-    template_files_list           = os.listdir  (template_files_directory)
-    print(                          'List of templates:')
+    template_files_directory = os.path.join(os.getcwd(),'templates')
+    template_files_list = os.listdir(template_files_directory)
+    print('List of templates:')
     for template_file_name in template_files_list:
-        print(                      template_files_list.index(template_file_name), ':', 
-                                    template_file_name)
-    template_selected_file_id     = int(input   ('Input number of selected template: '))
-    template_selected_file_path   = os.path.join(template_files_directory,
-                                                 template_files_list[template_selected_file_id])
-    template_selected_file_object = open        (template_selected_file_path, encoding='utf-8').read()
-    return                          template_selected_file_object
+        print(template_files_list.index(template_file_name), ':', template_file_name)
+    template_selected_file_id = int(input('Input number of selected template: '))
+    template_selected_file_path = os.path.join(template_files_directory,
+                                               template_files_list[template_selected_file_id])
+    template_selected_file_object = open(template_selected_file_path, encoding='utf-8').read()
+    return template_selected_file_object
  
-# Parse keys (variables) from the selected template
 def shmailer_template_parse(file_object):
+    '''Parse keys from the template'''
  
     template_enviroment = Environment()
-    template_ast        = template_enviroment.parse(file_object)
-    template_variables  = dict.fromkeys            (meta.find_undeclared_variables(template_ast), None)
-    return                template_variables
+    template_ast = template_enviroment.parse(file_object)
+    template_variables = dict.fromkeys (meta.find_undeclared_variables(template_ast), None)
+    return template_variables
  
-# Modify selected template
-def shmailer_template_modify        (file_object, template_variables):
+def shmailer_template_modify(file_object, template_variables):
+    '''Modify template'''
 
-    for    key in                    template_variables.keys():
-           template_variables[key] = input("Input '" + key + "': ")
-           template_file_modified  = Template(file_object).render(template_variables.items())
+    for key in template_variables.keys():
+        template_variables[key] = input("Input '" + key + "': ")
+        template_file_modified = Template(file_object).render(template_variables.items())
     return template_file_modified
 
-# Send email
 def shmailer_email_send(email_body):
+    '''Send email'''
 
-    print              ('Input additional information to send email!')
-    email_from        = input('From         : ')
-    email_to          = input('To           : ')
-    email_subject     = input('Email subject: ')
+    print('Input additional information to send email!')
+    email_from = input('From: ')
+    email_to = input('To: ')
+    email_subject = input('Email subject: ')
 
-    msg               = MIMEMultipart()
-    msg['From']       = email_from
-    msg['To']         = email_to
-    msg['Subject']    = email_subject
-    msg.attach(         MIMEText(email_body, 'html'))
+    msg = MIMEMultipart()
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = email_subject
+    msg.attach(MIMEText(email_body, 'html'))
   
-    server_smtp       = ''
-    server_port       = 0
-    server_login      = ''
-    server_password   = ''
+    server_smtp = ''
+    server_port = 0
+    server_login = ''
+    server_password = ''
 
-    server            = smtplib.SMTP(server_smtp, server_port)
-    server.starttls    ()
-    server.login       (server_login, server_password)
+    server = smtplib.SMTP(server_smtp, server_port)
+    server.starttls()
+    server.login(server_login, server_password)
     server.send_message(msg)
-    server.quit        ()
+    server.quit()
 
-    print              ('Email sent succesfully!')
-    return              True
+    print ('Email sent succesfully!')
+    return True
 
 
 # ======================== MAIN CYCLE ======================== #
 
+print('Hello, welcome to shmailer!')
 while(True):
+    '''Main cycle'''
 
-    shmailer_template_selected  = shmailer_template_select()
-    shmailer_template_variables = shmailer_template_parse (shmailer_template_selected)
-    shmailer_template_modified  = shmailer_template_modify(shmailer_template_selected, 
-                                                           shmailer_template_variables)
-    shmailer_send_result        = shmailer_email_send     (shmailer_template_modified)
-    shmailer_exit               = input                   ('press Enter')
-
+    shmailer_template_selected = shmailer_template_select()
+    shmailer_template_variables = shmailer_template_parse(shmailer_template_selected)
+    shmailer_template_modified = shmailer_template_modify(shmailer_template_selected, 
+                                                          shmailer_template_variables)
+    shmailer_send_result = shmailer_email_send(shmailer_template_modified)
+    shmailer_exit = input('press Enter')
+    #if shmailer_exit == 'exit':
+    #    break
+#print('Bye-bye!')
 
 # ======================== END =============================== #
